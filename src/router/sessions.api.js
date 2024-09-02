5a80d1e14fd5005613591a048743812f0d28bfbf
unknown <eservetti2018@gmail.com>
2024-09-01 23:35:41 -0300

    Agregado log in con google

--- src/router/sessions.api.js
+++ src/router/sessions.api.js
@@ -1,16 +1,62 @@
 import CustomRouter from "./customRouter.js";
-import { login,logout, register, data, verify } from "../controllers/controller.api.session.js";
-import passport from "../middlewares/passport.js"
-import validator from "../middlewares/joi.validator.js"
+import {
+  login,
+  logout,
+  register,
+  data,
+  verify,
+} from "../controllers/controller.api.session.js";
+import passport from "../middlewares/passport.js";
+import validator from "../middlewares/joi.validator.js";
 import { usersValidate } from "../schemas/users.validator.js";
 
 class SessionRouter extends CustomRouter {
   init() {
-    this.create("/register", ["PUBLIC"], validator(usersValidate),  passport.authenticate("register", {session: false}), register);
-    this.create("/login", ["PUBLIC"], passport.authenticate("login", {session: false}), login);
-    this.create("/data", ["USER", "ADMIN"], passport.authenticate("data", {session: false}), data);
+    this.create(
+      "/register",
+      ["PUBLIC"],
+      validator(usersValidate),
+      passport.authenticate("register", { session: false }),
+      register
+    );
+    this.create(
+      "/login",
+      ["PUBLIC"],
+      passport.authenticate("login", { session: false }),
+      login
+    );
+    this.create(
+      "/data",
+      ["USER", "ADMIN"],
+      passport.authenticate("data", { session: false }),
+      data
+    );
     this.create("/logout", ["USER", "ADMIN"], logout);
-    this.create("/verify/:email/:verifyCode", ["PUBLIC"], verify)
+    this.create("/verify/:email/:verifyCode", ["PUBLIC"], verify);
+    this.read(
+      "/google",
+      ["PUBLIC"],
+      passport.authenticate("Google", { scope: ["email", "profile"] })
+    );
+    this.read(
+      "/google/callback",
+      ["PUBLIC"],
+      passport.authenticate("Google", { session: false }),
+      (req, res, next) => {
+        try {
+          return res
+            .cookie("token", req.token, {
+              secure: true,
+              signedCookie: true,
+              maxAge: 3600000,
+              sameSite: "None",
+            })
+            .redirect("http://localhost:5173");
+        } catch (error) {
+          return next(error);
+        }
+      }
+    );
   }
 }
 
