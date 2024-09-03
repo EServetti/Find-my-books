84ae84072f921f3060a3a708216e9589960cf47c
unknown <eservetti2018@gmail.com>
2024-09-03 17:23:10 -0300

    Agregada funcion para leer un libro

--- src/controllers/controller.api.books.js
+++ src/controllers/controller.api.books.js
@@ -6,9 +6,13 @@ async function getBooks(req, res, next) {
         const { description } = req.body;
         let bookTitles = await getBooksFromChatGPT(description);
         const bookDetailsPromises = bookTitles.map((title) => getBookDetails(title));
+        
         const books = await Promise.all(bookDetailsPromises);
-
         const flattenedBooks = books.flat();
+
+        if(flattenedBooks.length === 0) {
+            return res.error404()
+        }
 
         return res.message200(flattenedBooks);
     } catch (error) {
@@ -16,4 +20,15 @@ async function getBooks(req, res, next) {
     }
 }
 
-export { getBooks };
+async function getOneBook(req, res, next) {
+    try {
+        const { title } = req.params;
+        let book = await getBookDetails(title);
+        book = book[0]
+        return res.message200(book);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export { getBooks, getOneBook };
--- src/router/books.api.js
+++ src/router/books.api.js
@@ -1,9 +1,10 @@
-import { getBooks } from "../controllers/controller.api.books.js";
+import { getBooks, getOneBook } from "../controllers/controller.api.books.js";
 import CustomRouter from "./customRouter.js";
 
 class BooksRouter extends CustomRouter {
     init() {
         this.create("/", ["PUBLIC"], getBooks)
+        this.create("/:title", ["PUBLIC"], getOneBook)
     }
 }
 
