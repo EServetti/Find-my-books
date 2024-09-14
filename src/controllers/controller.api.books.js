6fb1b645bf67a6290d6f25ca97dd7f5725a1e5c4
unknown <eservetti2018@gmail.com>
2024-09-14 00:56:44 -0300

    Cambiado el endpoint para leer libros compartidos y agregado endpoint para compartir un libro

--- src/controllers/controller.api.books.js
+++ src/controllers/controller.api.books.js
@@ -5,7 +5,7 @@ import getBooksFromChatGPT, {
 } from "../utils/openAI.js";
 import axios from "axios";
 import limitBooksVersion from "../utils/limitBooks.js"
-import { readOneService as readOneFriendService } from "../service/users.service.js";
+import {readService as readShared, createService as createSharedBook } from "../service/shared.service.js"
 
 async function getBooks(req, res, next) {
   try {
@@ -133,17 +133,43 @@ async function shared(req, res, next) {
   try {
     const {user} = req
     const {id} = req.params
-
-    const friend = await readOneFriendService(id)
-    const {sharedBooks} = friend
-    const sharedWithUser = sharedBooks.filter((b) => b.sharedWith === user._id)
-    if(sharedWithUser.length === 0) {
+    //libros compartidos con el usuario
+    const sharedBooks = await readShared({sharedBy: id})
+    const sharedWithUser = sharedBooks.filter((b) => b.sharedWith.toString() === user._id.toString())
+    //libros compartidos por el usuario
+    const userSharedBooks = await readShared({sharedBy: user._id})
+    const sharedWithFriend = userSharedBooks.filter((b) => b.sharedWith.toString() === id.toString())
+    if(sharedWithUser.length === 0 && sharedWithFriend.length === 0) {
       return res.error404()
     }
-    return res.message200(sharedWithUser)
+    //guardo todos los compartidos en un nuevo array
+    const sharedArray = []
+    for(const book of sharedWithUser) {
+      sharedArray.push(book)
+    }
+    for(const book of sharedWithFriend) {
+      sharedArray.push(book)
+    }
+    return res.message200(sharedArray)
   } catch (error) {
     return next(error)
   }
 }
 
-export { getBooks, getOneBook, create, destroy, update, shared };
+async function share(req, res, next) {
+  try {
+    const {user} = req
+    const {_id} = user
+    const {sharedWith, book_id } = req.body
+    const sharedBook = await createSharedBook({
+      sharedBy: _id,
+      sharedWith,
+      book_id
+    })
+    return res.message200("The book was shared")
+  } catch (error) {
+    return next(error)
+  }
+}
+
+export { getBooks, getOneBook, create, destroy, update, shared, share };
--- src/router/books.api.js
+++ src/router/books.api.js
@@ -1,4 +1,4 @@
-import { getBooks, getOneBook, create, destroy, update, shared } from "../controllers/controller.api.books.js";
+import { getBooks, getOneBook, create, destroy, update, shared, share } from "../controllers/controller.api.books.js";
 import CustomRouter from "./customRouter.js";
 import validator from "../middlewares/joi.validator.js";
 import { booksValidate } from "../schemas/books.validator.js";
@@ -8,6 +8,7 @@ class BooksRouter extends CustomRouter {
     init() {
         this.create("/", ["PUBLIC"], getBooks)
         this.create("/add", ["USER", "ADMIN"], validator(booksValidate), bookExists, create)
+        this.create("/share", ["USER", "ADMIN"], share)
         this.create("/:isbn", ["PUBLIC"], getOneBook)
         this.update("/:id", ["USER", "ADMIN"], update)
         this.destroy("/:id", ["USER", "ADMIN"], destroy)
