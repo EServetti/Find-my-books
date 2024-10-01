a2009439737c6f6542b59a2f71c9e2afa6a5d823
unknown <eservetti2018@gmail.com>
2024-10-01 13:36:12 -0300

    Configurado joi para que solo loguee el error

--- src/middlewares/joi.validator.js
+++ src/middlewares/joi.validator.js
@@ -1,20 +1,19 @@
-
 function validator(schema) {
-  try {
-    return (req, res, next) => {
-      const validation = schema.validate(req.body, {abortEarly : false})
-      if(validation.error) {
-        console.log(validation.error);
-        const message = validation.error.details.map((error) => error.message)
-        const error = new Error(message)
-        error.statusCode = 400
-        throw error
+  return (req, res, next) => {
+    try {
+      const validation = schema.validate(req.body, { abortEarly: false });
+      if (validation.error) {
+        console.log(validation.error.details.map((error) => error.message));
+        const message = validation.error.details.map((error) => error.message);
+        const error = new Error(message);
+        error.statusCode = 400;
+        throw error;
       }
-      return next()
+      return next();
+    } catch (error) {
+      return next(error);
     }
-  } catch (error) {
-    return next(error)
-  }
+  };
 }
 
-export default validator
+export default validator;
